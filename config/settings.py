d5f943a843e3d212958ba589a38bf31849aaf662
masa1203 <msys.sawada@gmail.com>
2019-06-29 01:20:54 +0900

    dotenvインポートできない

--- config/settings.py
+++ config/settings.py
@@ -12,13 +12,25 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from os.path import join, dirname
+
+# settings.py
 from dotenv import load_dotenv
+load_dotenv()
+
+# OR, the same with increased verbosity:
+load_dotenv(verbose=True)
+
+# OR, explicitly providing path to '.env'
+from pathlib import Path  # python3 only
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_NAME = os.path.basename(BASE_DIR)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
