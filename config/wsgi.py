9bbf0b626feabfcf59227494340d98292ab7de2d
masa1203 <msys.sawada@gmail.com>
2019-06-29 01:51:02 +0900

    collectstaticsした

--- config/wsgi.py
+++ config/wsgi.py
@@ -14,15 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-
-# settings.py
-from dotenv import load_dotenv
-load_dotenv()
-
-# OR, the same with increased verbosity:
-load_dotenv(verbose=True)
-
-# OR, explicitly providing path to '.env'
-from pathlib import Path  # python3 only
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
