4e089ace0e31391073fddae844accd1314e4de0b
masa1203 <msys.sawada@gmail.com>
2019-06-29 14:50:33 +0900

    import djagno_herokuを追加

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 from os.path import join, dirname
 from dotenv import load_dotenv
+
 load_dotenv(verbose=True)
 
 dotenv_path = join(dirname(__file__), '.env')
