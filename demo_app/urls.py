6e759fe545c046aa57828d53b5d4715d6bb72b0c
masa1203 <msys.sawada@gmail.com>
2019-06-30 17:12:13 +0900

    add base, input_form, result, hisotry page

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('input_form/', views.input_form, name='input_form'),
     path('result/', views.result, name='result'),
+    path('history/', views.history, name='history'),
 ]
--- demo_app/views.py
+++ demo_app/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import InputForm
 
-from sklearn.externals import joblib
+# from sklearn.externals import joblib
+import joblib
 import numpy as np
 from .models import Customer
 
 # 学習済みモデルの読み込み
 # 最初に読み込んだ方が一回で済む
 loaded_model = joblib.load('demo_app/demo_model.pkl')
-
 
 # Create your views here.
 def index(request):
@@ -19,7 +19,7 @@ def input_form(request):
         form = InputForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('demo_app:index') # /resultへ遷移するように変更
+            return redirect('demo_app:result') # /resultへ遷移するように変更
     else:
         form = InputForm()
         return render(request, 'demo_app/input_form.html', {'form':form})
@@ -29,7 +29,7 @@ def result(request):
     _data = Customer.objects.order_by('id').reverse().values_list\
         ('limit_balance', 'sex', 'education', 'marriage', 'age', 'pay_0', 'pay_2', 'pay_3', 'pay_4', 'pay_5', 'pay_6', 'bill_amt_1', 'pay_amt_1', 'pay_amt_2', 'pay_amt_3', 'pay_amt_4', 'pay_amt_5', 'pay_amt_6')
 
-    print(_data)
+    # print(_data)
     # 推論の実行
     x = np.array([_data[0]])
     y = loaded_model.predict(x)
@@ -47,9 +47,36 @@ def result(request):
             comment = 'この方への貸し出しは全く問題ありません'
         else:
             comment = 'この方への貸し出しは問題ないでしょう'
+
+    # 推論結果をDBに保存
+    _customer = Customer.objects.order_by('id').reverse()[0] # Customerの切り出し
+    _customer.proba = y_proba[y]
+    _customer.result = y
+    _customer.comment = comment
+    _customer.save() # データを保存
+
     contexts = {
         'y':y,
         'y_proba':round(y_proba[y], 2),
         'comment': comment,
     }
     return render(request, 'demo_app/result.html', contexts)
+
+def history(request):
+    if request.method == 'POST':
+        print(request)
+        print(request.POST)
+        d_id = request.POST # POSTされた値を取得
+        d_customer = Customer.objects.get(id=d_id['d_id'])
+        d_customer.delete() # 顧客データを消去
+        customers = Customer.objects.all()
+        contexts = {
+            'customers': customers
+        }
+        return render(request, 'demo_app/history.html', contexts)
+    else:        
+        customers = Customer.objects.all() # 顧客データの取得
+        contexts = {
+            'customers': customers
+        }
+        return render(request, 'demo_app/history.html', contexts)
